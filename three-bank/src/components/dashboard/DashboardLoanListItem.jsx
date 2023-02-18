d40cf532e36fe2ec3c7903da772aa5703ed4a5ad
Killjoy <aryaajitnair@gmail.com>
2023-02-18 15:06:58 +0530

    feat: Add loan request page

---
+++ three-bank/src/components/dashboard/DashboardLoanListItem.jsx
@@ -0,0 +1,57 @@
+import { Box, Button, Paper, Typography } from "@mui/material";
+import React, { useState } from "react";
+import CustomModal from "../admin/CustomModal";
+const DashboardLoanListItem = (props) => {
+	const item = props;
+	const [expand, setExpand] = useState(false);
+
+	const [loading, setLoading] = useState(false);
+
+	const requestCreditData = () => {
+		setExpand((prev) => !prev);
+	};
+
+	return (
+		<>
+			<CustomModal open={expand} handleClose={(e, r) => setExpand(false)} />
+
+			<Box
+				sx={{
+					display: "flex",
+					direction: "column",
+					justifyContent: "flex-start",
+					marginBottom: "1vh",
+					borderRadius: "1vh",
+					paddingY: "3vh",
+					paddingX: "2vw",
+					mb: 3,
+				}}
+				component={Paper}
+				// elevation={6}
+				// className={"awesome-bg-0"}
+			>
+				<Box
+					sx={{
+						display: "flex",
+						flexDirection: "row",
+						justifyContent: "space-between",
+						marginX: "2%",
+						height: "100%",
+						flexGrow: 1,
+						gap: "1.5vh",
+					}}
+				>
+					<Typography variant="body1">Loan ID: {item.loanId}</Typography>
+					<Button variant="contained" disabled>
+						Status: {item.approvedStatus}
+					</Button>
+				</Box>
+				<Box>
+					<Typography>Loan Amount:{item.loanAmmount}</Typography>
+				</Box>
+			</Box>
+		</>
+	);
+};
+
+export default DashboardLoanListItem;
--- three-bank/src/screens/dashboard/DashboardLoan.jsx
+++ three-bank/src/screens/dashboard/DashboardLoan.jsx
@@ -1,9 +1,143 @@
-import React from 'react'
+import { useAuth } from "@arcana/auth-react";
+import { Box, Typography, TextField, Paper, Button } from "@mui/material";
+import {
+    collection,
+    getDocs,
+    query,
+    updateDoc,
+    where,
+    doc,
+    arrayUnion,
+} from "firebase/firestore";
+import React, { createContext, useEffect, useState } from "react";
+import DashboardLoanListItem from "../../components/dashboard/DashboardLoanListItem";
+import { db } from "../../config/firebase";
+
+// const loanRequests = [
+//     {
+//         loanId: "loan 1",
+//         loanAmmount: 3000,
+//         approvedStatus: "approved",
+//         timestamp: new Date(),
+//     },
+//     {
+//         loanId: "loan 2",
+//         loanAmmount: 4000,
+//         approvedStatus: "rejected",
+//         timestamp: new Date(),
+//     },
+//     {
+//         loanId: "loan 3",
+//         loanAmmount: 4000,
+//         approvedStatus: "waiting",
+//         timestamp: new Date(),
+//     },
+// ];
 
 const DashboardLoan = () => {
-  return (
-    <div>DashboardLoan</div>
-  )
-}
+    const [loanVal, setLoanVal] = useState(0);
+    const [loanTenure, setLoanTenure] = useState(0);
+    const [loanRequests, setLoanRequest] = useState([]);
 
-export default DashboardLoan
+    const auth = useAuth();
+
+    useEffect(() => {
+        (async () => {
+            const q = query(
+                collection(db, "ThreeBank"),
+                where("arcanaUid", "==", auth.user.publicKey)
+            );
+
+            const querySnapshot = await getDocs(q);
+            const dataArr = [];
+
+            querySnapshot.forEach((doc) => {
+                dataArr.push({ id: doc.id, ...doc.data() });
+            });
+            setLoanRequest(dataArr[0].loanRequest);
+        })();
+    }, [auth]);
+
+    const handleLoanRequest = async () => {
+        if (!auth.user.publicKey) {
+            alert("Arcana nai aya");
+            return;
+        }
+        const q = query(
+            collection(db, "ThreeBank"),
+            where("arcanaUid", "==", auth.user.publicKey)
+        );
+
+        const data = await getDocs(q);
+        let datafromfirebase = [];
+        data.forEach((doc) => {
+            datafromfirebase.push({ id: doc.id, ...doc.data() });
+        });
+
+        const accountRef = doc(db, "ThreeBank", datafromfirebase[0].id);
+        await updateDoc(accountRef, {
+            loanRequest: arrayUnion({
+                loanId: (Math.random() * 100).toString(),
+                loanAmmount: loanVal,
+                loanTenure: loanTenure,
+                approvedStatus: "waiting",
+                timestamp: new Date(),
+            }),
+        });
+    };
+
+    return (
+        <Box>
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "flex-start",
+                    marginBottom: "1vh",
+                    borderRadius: "1vh",
+                    paddingY: "3vh",
+                    paddingX: "2vw",
+                    mb: 3,
+                }}
+                component={Paper}
+                // elevation={6}
+                // className={"awesome-bg-0"}
+            >
+                <Typography>Loan Details</Typography>
+            </Box>
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "flex-start",
+                    marginBottom: "1vh",
+                    borderRadius: "1vh",
+                    paddingY: "3vh",
+                    paddingX: "2vw",
+                    mb: 3,
+                }}
+                component={Paper}
+                // elevation={6}
+                // className={"awesome-bg-0"}
+            >
+                <TextField
+                    placeholder="Enter Loan Amount"
+                    label="Loan Amount"
+                    value={loanVal}
+                    onChange={(e) => setLoanVal(e.target.value)}
+                ></TextField>
+                <TextField
+                    placeholder="Enter Loan tenure"
+                    label="Loan Tenure"
+                    value={loanTenure}
+                    onChange={(e) => setLoanTenure(e.target.value)}
+                ></TextField>
+                <Button onClick={handleLoanRequest}>Request Loan</Button>
+            </Box>
+            <Typography>Loan Requests</Typography>
+            {loanRequests.map((item) => (
+                <DashboardLoanListItem {...item} />
+            ))}
+        </Box>
+    );
+};
+
+export default DashboardLoan;
