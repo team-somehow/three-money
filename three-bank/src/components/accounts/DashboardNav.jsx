ff2cacfd5153b8c42643dffaab37ac3b726871e5
Killjoy <aryaajitnair@gmail.com>
2023-02-18 15:03:39 +0530

    feat: Add nav for dashboard:

---
+++ three-bank/src/components/accounts/DashboardNav.jsx
@@ -0,0 +1,76 @@
+import {
+    Button,
+    Divider,
+    Drawer,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Toolbar,
+    Typography,
+} from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+
+function DashboardNav({ routes }) {
+    return (
+        <Drawer
+            variant="permanent"
+            sx={{
+                position: "relative",
+                // display: { xl: "initial", sm: "none", xs: "none" },
+                width: 300,
+                flexShrink: 0,
+                "& .MuiDrawer-paper": {
+                    width: 300,
+                    py: 4,
+                    boxSizing: "border-box",
+                },
+            }}
+        >
+            <Toolbar>
+                <img></img>
+                <Link to="/dashboard">
+                    <Typography variant="h3">3 Money</Typography>
+                </Link>
+            </Toolbar>
+            <Divider />
+            <List>
+                {routes.map((r) => (
+                    <Link to={r.path} key={r.headingText}>
+                        <ListItem key={r.headingText} disablePadding>
+                            <ListItemButton sx={{ p: 2, px: 4 }}>
+                                <ListItemIcon
+                                    sx={{
+                                        fontSize: { xl: "30px", sm: "15px" },
+                                        color: "black",
+                                    }}
+                                >
+                                    {r.mainIcon}
+                                </ListItemIcon>
+                                <ListItemText
+                                    sx={{ fontSize: "24px" }}
+                                    primary={r.headingText}
+                                />
+                            </ListItemButton>
+                        </ListItem>
+                    </Link>
+                ))}
+            </List>
+            <Button
+                sx={{
+                    position: "absolute",
+                    bottom: "10px",
+                    color: "red",
+                }}
+                variant="text"
+                fullWidth
+            >
+                Logout
+            </Button>
+        </Drawer>
+    );
+}
+
+export default DashboardNav;
--- three-bank/src/screens/dashboard/Dashboard.jsx
+++ three-bank/src/screens/dashboard/Dashboard.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import DashboardNav from "../../components/accounts/DashboardNav";
+import { Box } from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+    const navigate = useNavigate();
 
-export default Dashboard
+    return (
+        <Box sx={{ display: "flex" }}>
+            <DashboardNav
+                routes={[
+                    {
+                        headingText: "My Account",
+                        path: "/dashboard/details",
+                        mainIcon: <AccountCircleIcon fontSize="large" />,
+                    },
+                    {
+                        headingText: "Apply Loan",
+                        path: "/dashboard/loan",
+                        mainIcon: <MonetizationOnIcon fontSize="large" />,
+                    },
+                    {
+                        headingText: "My Loans",
+                        path: "/dashboard/loan-payment",
+                        mainIcon: <MonetizationOnIcon fontSize="large" />,
+                    },
+                ]}
+            />
+            <Outlet />
+        </Box>
+    );
+};
+
+export default Dashboard;
