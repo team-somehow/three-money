78f8cc8762497e64a3afa33bd680eeee4e4fcdab
Killjoy <aryaajitnair@gmail.com>
2023-02-18 14:52:58 +0530

    feat: configure dashboard

--- three-bank/src/screens/Home/Home.jsx
+++ three-bank/src/screens/Home/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Auth } from "@arcana/auth-react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+    const navigate = useNavigate();
 
-export default Home
+    return (
+        <div>
+            <div>
+                <h1 style={{ textAlign: "center", marginBlock: "50px" }}>
+                    Welcome to 3 Bank
+                </h1>
+                <Auth
+                    externalWallet={false}
+                    theme="dark"
+                    onLogin={() => {
+                        navigate("/account/create");
+                    }}
+                ></Auth>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
