735e6c7cc72952fade4eb8848d808120f234ec6f
Killjoy <aryaajitnair@gmail.com>
2023-02-18 15:01:50 +0530

    feat: Add approval screen

---
+++ three-bank/src/components/admin/AdminApprovalListItem.jsx
@@ -0,0 +1,144 @@
+import { Box, Button, Paper, Typography } from "@mui/material";
+import React, { useState } from "react";
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
+import KeyboardDoubleArrowUpIcon from "@mui/icons-material/KeyboardDoubleArrowUp";
+import { doc, updateDoc } from "@firebase/firestore";
+import { db } from "../../config/firebase";
+import { arcanaProvider } from "../../main";
+import contractAddress from "../../constants/contractAddress";
+import { providers, Contract, utils } from "ethers";
+// import ThreeBank from "../../artifacts/contracts/ThreeBank.sol/ThreeBank.json";
+
+const AdminApprovalListItem = (props) => {
+    // const provider = new providers.Web3Provider(arcanaProvider.provider);
+    // const signer = provider.getSigner();
+    // const contract = new Contract(contractAddress, ThreeBank.abi, signer);
+    const item = props;
+    console.log(contractAddress);
+    const [expand, setExpand] = useState(false);
+
+    const [loading, setLoading] = useState(false);
+
+    const approve = async () => {
+        await arcanaProvider.connect();
+        const personalData = {
+            name: item.name,
+            dateOfBirth: "01-01-1980",
+            gender: "Male",
+            panNumber: item.panCardNumber,
+        };
+
+        await contract.enroll(personalData, personalData.panNumber);
+
+        await updateDoc(doc(db, "ThreeBank", item.id), {
+            isAccountApproved: true,
+        });
+    };
+
+    return (
+        <>
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "flex-start",
+                    marginBottom: "1vh",
+                    borderRadius: "1vh",
+                    paddingY: "3vh",
+                    paddingX: "2vw",
+                    mb: 3,
+                }}
+                component={Paper}
+                // elevation={6}
+                // className={"awesome-bg-0"}
+            >
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "space-around",
+                        marginX: "2%",
+                        height: "100%",
+                        flexGrow: 1,
+                        gap: "1.5vh",
+                    }}
+                >
+                    <Typography variant="h4">{item.name}</Typography>
+                    <Typography variant="body1">{item.city}</Typography>
+                    {expand && (
+                        <>
+                            <Typography variant="body1">
+                                Father/Husband Name: {item.FatherName}
+                            </Typography>
+                            <Typography variant="body1">
+                                Address: {item.address}
+                            </Typography>
+                            <Typography variant="body1">
+                                PanCard Number: {item.panCardNumber}
+                            </Typography>
+                            <Typography variant="body1">
+                                Mobile Number: {item.mobileNumber}
+                            </Typography>
+
+                            <a href={item.panCard} target="_blank">
+                                <Button fullWidth variant="outlined">
+                                    PanCard
+                                </Button>
+                            </a>
+                            <a href={item.addressProof} target="_blank">
+                                <Button fullWidth variant="outlined">
+                                    Address Proof
+                                </Button>
+                            </a>
+                            <a href={item.SignatureProof} target="_blank">
+                                <Button fullWidth variant="outlined">
+                                    Signature Proof
+                                </Button>
+                            </a>
+                            <a href={item.IncomeProof} target="_blank">
+                                <Button fullWidth variant="outlined">
+                                    Income Proof
+                                </Button>
+                            </a>
+                        </>
+                    )}
+                </Box>
+                <Box width="20%">
+                    <Button
+                        fullWidth
+                        onClick={() => setExpand(!expand)}
+                        sx={{
+                            marginTop: "10%",
+                        }}
+                        variant="outlined"
+                        endIcon={
+                            !expand ? (
+                                <KeyboardDoubleArrowDownIcon />
+                            ) : (
+                                <KeyboardDoubleArrowUpIcon />
+                            )
+                        }
+                    >
+                        {expand ? `Collapse` : `Expand`}
+                    </Button>
+                    {expand && (
+                        <Button
+                            fullWidth
+                            onClick={() => {
+                                approve();
+                            }}
+                            sx={{
+                                marginTop: "10%",
+                            }}
+                            variant="contained"
+                            disabled={loading}
+                        >
+                            Verify
+                        </Button>
+                    )}
+                </Box>
+            </Box>
+        </>
+    );
+};
+
+export default AdminApprovalListItem;
--- three-bank/src/screens/Admin/AdminApproval.jsx
+++ three-bank/src/screens/Admin/AdminApproval.jsx
@@ -1,9 +1,160 @@
-import React from 'react'
+import {
+    Avatar,
+    Box,
+    Container,
+    List,
+    ListItemAvatar,
+    Typography,
+    ListItem,
+    ListItemText,
+    Divider,
+    Card,
+    Button,
+    Paper,
+    Grid,
+    ListItemIcon,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+// import { collection, getDocs } from "firebase/firestore";
+// import { db } from "../../config/firebase";
+import { useNavigate } from "react-router-dom";
+import AdminApprovalListItem from "../../components/admin/AdminApprovalListItem";
+import { useAuth } from "@arcana/auth-react";
+import { collection, getDocs, query, where } from "@firebase/firestore";
+import { db } from "../../config/firebase";
 
-const AdminApproval = () => {
-  return (
-    <div>AdminApproval</div>
-  )
+function AdminApproval() {
+    const [data, setData] = useState([]);
+    const [walletAddress, setWalletAddress] = useState("");
+    const [approved, setApproved] = useState(8);
+    const [pending, setPending] = useState(7);
+    const navigate = useNavigate();
+    const auth = useAuth();
+    const [loading, setLoading] = useState();
+
+    // _________________________________________
+    // TODO: approve karne par
+    // arcanaUid se ko ThreeCredit se lo aur
+    // isAccountApproved ko true karo
+
+    // ye karne ke baad Credit details waale mao thoos do
+    // _________________________________________
+
+    useEffect(() => {
+        (async () => {
+            setLoading(true);
+
+            if (!auth) return;
+            if (!auth?.user) return;
+            if (!auth.user?.publicKey) return;
+
+            console.log(auth?.user?.publicKey);
+
+            const q = query(collection(db, "ThreeBank"));
+            const querySnapshot = await getDocs(q);
+
+            const dataFromFb = [];
+            querySnapshot.forEach((doc) => {
+                const loopWaalaData = doc.data();
+                dataFromFb.push({
+                    id: doc.id,
+                    name: loopWaalaData.personalDetails.name,
+                    FatherName: loopWaalaData.personalDetails.fatherName,
+                    address: loopWaalaData.personalDetails.address,
+                    panCardNumber: loopWaalaData.personalDetails.pan,
+                    mobileNumber: loopWaalaData.personalDetails.mobile,
+                    panCard: loopWaalaData.personalDetails.documents.panCardUrl,
+                    addressProof:
+                        loopWaalaData.personalDetails.documents.addressProofUrl,
+                    SignatureProof:
+                        loopWaalaData.personalDetails.documents
+                            .signatureProofUrl,
+                    IncomeProof:
+                        loopWaalaData.personalDetails.documents.incomeProofUrl,
+                });
+            });
+            setData(dataFromFb);
+            console.log(dataFromFb);
+        })();
+    }, [auth]);
+
+    return (
+        <Box
+            component={Paper}
+            width={"97vw"}
+            elevation={12}
+            sx={{
+                maxHeight: "96vh",
+                height: "96vh",
+                borderRadius: "1vw",
+                paddingX: "1.5vw",
+                display: "flex",
+                flexDirection: "row",
+                marginY: "1.5vh",
+                marginX: "1.5vw",
+                overflowY: "scroll",
+                paddingY: "4vh",
+            }}
+            className="awesome-bg-0 "
+        >
+            <Box width={"20%"}>
+                {/* <img
+                    src="/logo.png"
+                    style={{
+                        height: "10vh",
+                        // width: "100%",
+                        margin:"auto"
+                    }}
+                /> */}
+                <div
+                    style={{
+                        paddingLeft: "18px",
+                        marginTop: "12px",
+                        marginBottom: "28px",
+                        cursor: "pointer",
+                        display: "flex",
+                        alignItems: "center",
+                    }}
+                >
+                    <ListItem onClick={() => navigate("/")}>
+                        <ListItemIcon>
+                            <img src="/logo.png" width={"40px"} />
+                        </ListItemIcon>
+                        <ListItemText>
+                            <h2>3 Bank</h2>
+                        </ListItemText>
+                    </ListItem>
+                </div>
+            </Box>
+            <Box
+                width={"80%"}
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "flex-start",
+                    gap: "2vh",
+                    paddingX: "5%",
+                }}
+            >
+                <Box
+                    component={Paper}
+                    sx={{
+                        backgroundColor: "white",
+                        padding: "2%",
+                        textAlign: "center",
+                        borderRadius: "0.5vw",
+                        marginBottom: "4.5vh",
+                        height: "10vh",
+                    }}
+                >
+                    <Typography variant="h4">Admin Portal</Typography>
+                </Box>
+                {data.map((item) => (
+                    <AdminApprovalListItem {...item} />
+                ))}
+            </Box>
+        </Box>
+    );
 }
 
-export default AdminApproval
+export default AdminApproval;
