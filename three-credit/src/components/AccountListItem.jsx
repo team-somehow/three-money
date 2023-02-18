7a513269dcc5dea0acda841166c1012f920a9184
Vinay <vinaykanse9451@gmail.com>
2023-02-18 11:10:49 +0530

    feat: add AccountListItem, DetailsCard components

---
+++ three-credit/src/components/AccountListItem.jsx
@@ -0,0 +1,77 @@
+import { Box, Divider, Typography } from "@mui/material";
+import React from "react";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
+import { Link } from "react-router-dom";
+
+function AccountListItem({
+  icon,
+  bankName,
+  cardNumber,
+  isActive,
+  enquiryDate,
+  onClickHandle,
+}) {
+  return (
+    <Box my={2} mx={4}>
+      <Box
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+      >
+        <Box display={"flex"} alignItems={"center"}>
+          <Box>
+            {icon ? (
+              <img
+                style={{
+                  width: "60px",
+                }}
+                src={`${icon}`}
+              />
+            ) : (
+              <AccountBalanceIcon
+                color="primary"
+                sx={{
+                  fontSize: "42px",
+                }}
+              />
+            )}
+          </Box>
+          <Box ml={3}>
+            <Typography variant="h6" fontWeight={"bold"}>
+              {bankName}
+            </Typography>
+            <Box display={"flex"}>
+              <Typography>Loan ID: </Typography>
+              <Typography>{cardNumber}</Typography>
+            </Box>
+            <Box display={"flex"}>
+              <Typography>
+                {enquiryDate ? "Enquired on: " : "Status: "}
+              </Typography>
+              <Typography
+                color={isActive ? "green" : "red"}
+                fontWeight={"bold"}
+              >
+                {enquiryDate ? enquiryDate : isActive ? " Active" : " Pending"}
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
+
+        {onClickHandle ? (
+          <div onClick={onClickHandle} style={{ cursor: "pointer" }}>
+            <ArrowForwardIosIcon />
+          </div>
+        ) : (
+          <Box>
+            <ArrowForwardIosIcon />
+          </Box>
+        )}
+      </Box>
+      <Divider />
+    </Box>
+  );
+}
+
+export default AccountListItem;
---
+++ three-credit/src/components/DetailsCard.jsx
@@ -0,0 +1,71 @@
+import { Box, Divider, Paper, Typography } from "@mui/material";
+import React from "react";
+
+function DetailsCard({
+  mainHeading,
+  lastUpdated,
+  label,
+  labelBackground,
+  leftHeadingNumber,
+  leftHeadingTitle,
+  rightHeadingNumber,
+  rightHeadingTitle,
+}) {
+  return (
+    <Box
+      width={"100%"}
+      component={Paper}
+      m={"auto"}
+      my={4}
+      boxShadow={"0 0 10px 0 rgba(0, 0, 0, 0.1)"}
+      borderRadius={"25px"}
+    >
+      <Box
+        display={"flex"}
+        alignItems={"center"}
+        pt={3}
+        justifyContent={"space-between"}
+      >
+        <Box px={3} py={2}>
+          <Typography variant="h4" fontWeight={"bold"}>
+            {mainHeading}
+          </Typography>
+          <Typography>
+            Last updated on <b>{lastUpdated}</b>
+          </Typography>
+        </Box>
+        <Box
+          bgcolor={labelBackground}
+          sx={{
+            p: 1,
+            pl: 2,
+            borderTopLeftRadius: "10px",
+            borderBottomLeftRadius: "10px",
+          }}
+        >
+          <Typography color={"white"} px={3}>
+            {label}
+          </Typography>
+        </Box>
+      </Box>
+      <Divider />
+      <Box display={"flex"}>
+        <Box width={"50%"} ml={3} py={3}>
+          <Typography variant="h4" fontWeight={"bold"}>
+            {leftHeadingNumber}
+          </Typography>
+          <Typography variant="h6">{leftHeadingTitle}</Typography>
+        </Box>
+        <Divider orientation="vertical" flexItem />
+        <Box width={"50%"} ml={3} py={3}>
+          <Typography variant="h4" fontWeight={"bold"}>
+            {rightHeadingNumber}
+          </Typography>
+          <Typography variant="h6">{rightHeadingTitle}</Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default DetailsCard;
