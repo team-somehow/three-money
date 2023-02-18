b704f19d614d406d8dbf6d60456ff013e3cfa483
Vinay <vinaykanse9451@gmail.com>
2023-02-19 03:02:34 +0530

    feat: update styling of DashboardLoanListItem

--- three-bank/src/components/dashboard/DashboardLoanListItem.jsx
+++ three-bank/src/components/dashboard/DashboardLoanListItem.jsx
@@ -23,36 +23,41 @@ const DashboardLoanListItem = (props) => {
                     display: "flex",
                     direction: "column",
                     justifyContent: "flex-start",
-                    marginBottom: "1vh",
                     borderRadius: "1vh",
-                    paddingY: "3vh",
-                    paddingX: "2vw",
-                    mb: 3,
+                    px: 4,
+                    py: 2,
+                    my: 2,
+                    "&:hover": {
+                        boxShadow: "0 4px 20px rgba(0, 0, 0, 0.08)",
+                    },
                 }}
                 component={Paper}
-                // elevation={6}
-                // className={"awesome-bg-0"}
             >
                 <Box
                     sx={{
                         display: "flex",
                         flexDirection: "row",
                         justifyContent: "space-between",
-                        marginX: "2%",
-                        height: "100%",
-                        flexGrow: 1,
-                        gap: "1.5vh",
+                        width: "100%",
                     }}
                 >
-                    <Typography variant="body1">
-                        Loan ID: {item.loanId}
-                    </Typography>
+                    <Box>
+                        <Typography variant="h4">
+                            Loan ID: {item.loanId}
+                        </Typography>
+                        <Typography
+                            variant="h6"
+                            display={"flex"}
+                            alignItems={"center"}
+                        >
+                            Loan Amount:
+                            <img src="/assets/matic.png" width={"40px"} />
+                            {item.loanAmmount}
+                        </Typography>
+                    </Box>
                     <Button variant="contained" disabled>
                         Status: {item.approvedStatus}
                     </Button>
-                </Box>
-                <Box>
-                    <Typography>Loan Amount:{item.loanAmmount}</Typography>
                 </Box>
             </Box>
         </>
