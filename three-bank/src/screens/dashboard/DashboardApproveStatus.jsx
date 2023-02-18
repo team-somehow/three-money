c31f455923566acad0e318948815b8dac81eb2df
Killjoy <aryaajitnair@gmail.com>
2023-02-18 15:04:22 +0530

    feat: Add dashboard approve status

--- three-bank/src/screens/dashboard/DashboardApproveStatus.jsx
+++ three-bank/src/screens/dashboard/DashboardApproveStatus.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { CircularProgress } from "@mui/material";
+import { useAuth } from "@arcana/auth-react";
+import { collection, getDocs, query, where } from "@firebase/firestore";
+import { db } from "../../config/firebase";
+import { Box, Typography } from "@mui/material";
 
 const DashboardApproveStatus = () => {
-  return (
-    <div>DashboardApproveStatus</div>
-  )
-}
+    const auth = useAuth();
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState();
 
-export default DashboardApproveStatus
+    useEffect(() => {
+        (async () => {
+            setLoading(true);
+
+            if (!auth) return;
+            if (!auth?.user) return;
+            if (!auth.user?.publicKey) return;
+
+            console.log(auth?.user?.publicKey);
+
+            const q = query(
+                collection(db, "ThreeBank"),
+                where("arcanaUid", "==", auth?.user?.publicKey)
+            );
+
+            const querySnapshot = await getDocs(q);
+            let threeBanksUserData = null;
+
+            querySnapshot.forEach((doc) => {
+                console.log(doc.id, " => ", doc.data());
+                threeBanksUserData = doc.data();
+            });
+
+            // let approvedByBank = true;
+            let approvedByBank = threeBanksUserData?.isAccountApproved;
+
+            console.log("approvedByBank", approvedByBank);
+
+            if (approvedByBank) {
+                navigate("/dashboard/details");
+            } else {
+                navigate("/dashboard/approved-status");
+            }
+            setLoading(false);
+        })();
+    }, [auth]);
+
+    if (loading)
+        return (
+            <div className="center-container">
+                <CircularProgress />
+            </div>
+        );
+
+    return (
+        <Box
+            sx={{
+                width: "100%",
+                height: "100vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Typography variant="h2">
+                Your Account is Not yet Approved
+            </Typography>
+        </Box>
+    );
+};
+
+export default DashboardApproveStatus;
