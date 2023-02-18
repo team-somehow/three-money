f3ccf2424a68f6c738cf09a9717b50ad05b5b40d
Vinay <vinaykanse9451@gmail.com>
2023-02-18 18:10:53 +0530

    add animations and ui fixes

--- three-credit/src/screens/DashboardHome/DashboardHome.jsx
+++ three-credit/src/screens/DashboardHome/DashboardHome.jsx
@@ -2,12 +2,12 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import LineChart from "../../components/LineChart";
 import {
-  Box,
-  Button,
-  Container,
-  dividerClasses,
-  Paper,
-  Typography,
+    Box,
+    Button,
+    Container,
+    dividerClasses,
+    Paper,
+    Typography,
 } from "@mui/material";
 import SummaryDetails from "../../components/SummaryDetails";
 import ThreeCredit from "../../artifacts/contracts/ThreeCredit.sol/ThreeCredit.json";
@@ -19,83 +19,95 @@ import { providers, Contract, utils } from "ethers";
 import { arcanaProvider } from "../../main";
 import contractAddress from "../../constants/contractAddress";
 import { useEffect } from "react";
+import CachedIcon from "@mui/icons-material/Cached";
 
 const DashboardHome = () => {
-  const provider = new providers.Web3Provider(arcanaProvider.provider);
-  const signer = provider.getSigner();
-  const contract = new Contract(contractAddress, ThreeCredit.abi, signer);
+    const provider = new providers.Web3Provider(arcanaProvider.provider);
+    const signer = provider.getSigner();
+    const contract = new Contract(contractAddress, ThreeCredit.abi, signer);
 
-  const creditDataCtx = useContext(CreditDataContext);
-  const [creditScore, setCreditScore] = useState(
-    creditDataCtx.userCreditRequest[0].creditScore
-  );
+    const creditDataCtx = useContext(CreditDataContext);
+    const [creditScore, setCreditScore] = useState(
+        creditDataCtx.userCreditRequest[0].creditScore
+    );
 
-  const updateScore = async () => {
-    await arcanaProvider.connect();
-    const result = await contract.calculateCreditScore(creditDataCtx.pan);
-    console.log(result);
-  };
+    const updateScore = async () => {
+        await arcanaProvider.connect();
+        const result = await contract.calculateCreditScore(creditDataCtx.pan);
+        console.log(result);
+    };
 
-  if (!creditDataCtx?.userCreditRequest)
-    return <div>account to bana le bhai</div>;
+    if (!creditDataCtx?.userCreditRequest)
+        return <div>account to bana le bhai</div>;
 
-  return (
-    <Box sx={{ display: "flex", flexDirection: "column" }}>
-      <Box display={"flex"} alignItems={"center"}>
-        <Box
-          component={Paper}
-          p={2}
-          width={"60%"}
-          height={"60vh"}
-          m={"auto"}
-          mt={6}
-          mx={4}
-        >
-          <Typography variant="h2" py={2}>
-            Your Score History
-          </Typography>
-          <LineChart />
-        </Box>
-        <Box
-          component={Paper}
-          p={2}
-          width={"40%"}
-          height={"60vh"}
-          m={"auto"}
-          mt={6}
-          mx={4}
-          display={"flex"}
-          justifyContent={"center"}
-          alignContent={"space-evenly"}
-          flexDirection={"column"}
-        >
-          <Typography variant="h3" align="center" py={2}>
-            Request Credit Score
-          </Typography>
-          <CreditMeter creditScore={creditScore} />
-          <Typography align="center" variant="h5" mb={2}>
-            Last Updated:{" "}
-            <b>
-              {JSON.stringify(creditDataCtx.userCreditRequest[0].timestamp)}
-            </b>
-          </Typography>
-          <Button
-            variant="contained"
-            sx={{ bgcolor: "#618EF6", color: "white" }}
-            onClick={updateScore}
-          >
-            Update Score
-          </Button>
-        </Box>
-      </Box>
-      <SummaryDetails links={links} />
-      {/* <Link to="/dashboard/">Home</Link>
+    return (
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <Box display={"flex"} alignItems={"center"}>
+                <Box
+                    component={Paper}
+                    p={2}
+                    width={"60%"}
+                    height={"60vh"}
+                    m={"auto"}
+                    mt={6}
+                    mx={4}
+                >
+                    <Typography variant="h2" py={2}>
+                        Your Score History
+                    </Typography>
+                    <LineChart />
+                </Box>
+                <Box
+                    component={Paper}
+                    p={2}
+                    width={"40%"}
+                    height={"60vh"}
+                    m={"auto"}
+                    mt={6}
+                    mx={4}
+                    display={"flex"}
+                    justifyContent={"center"}
+                    alignContent={"space-evenly"}
+                    flexDirection={"column"}
+                >
+                    <Typography variant="h3" align="center" py={2}>
+                        Request Credit Score
+                    </Typography>
+                    <CreditMeter creditScore={creditScore} />
+                    <Typography align="center" variant="h5" mb={2}>
+                        Last Updated:{" "}
+                        <b>
+                            {JSON.stringify(
+                                new Date(
+                                    creditDataCtx.userCreditRequest[0].timestamp
+                                ).toDateString()
+                            )}
+                        </b>
+                    </Typography>
+                    <Button
+                        variant="contained"
+                        sx={{
+                            width: "50%",
+                            py: 2,
+                            bgcolor: "#618EF6",
+                            color: "white",
+                            mx: "auto",
+                        }}
+                        onClick={updateScore}
+                        startIcon={<CachedIcon />}
+                    >
+                        Update Score
+                    </Button>
+                </Box>
+            </Box>
+            <SummaryDetails links={links} />
+            {/* <Link to="/dashboard/">Home</Link>
       <Link to="/dashboard/details">Dashboard Details</Link>
       {links.map((link) => (
         <Link to={link.path}>{link.name}</Link>
       ))} */}
-    </Box>
-  );
+        </Box>
+    );
 };
 
 export default DashboardHome;
